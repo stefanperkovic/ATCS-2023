1710ef2d885977683aec73666a46cc0839bc27be
stefanperkovic <stefan.perkovic@menloschool.org>
2023-12-04 13:17:48 -0800

    Created car and game files

---
+++ Semester 1 Final Project/car.py
@@ -0,0 +1,29 @@
+# Stefan Perkovic
+import pygame
+
+
+class Car:
+
+
+    def __init__(self):
+        self.x = 100
+        self.y_ = 100
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.angle = 0
+
+
+
+# When W or UP Error
+if self.velocity_y < 50:
+    self.velocity_y += 10
+
+# When D or Right Error 
+if self.velocity_x < 30
+    self.velocity_x += 10
+
+# When A or Left Error
+if self.velocity_x < -30
+    self.velococif
+
+
